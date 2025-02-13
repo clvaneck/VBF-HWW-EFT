0035a9afb7354131e68cc358cd551e4eb69e4117
clvaneck <77339672+clvaneck@users.noreply.github.com>
2025-02-13 12:31:42 +0100

    removed double equal sign

--- Madgraph/MadGraph-JobOptions/Signal-processes-no-cuts/100007/mc.aMC_qq_H-100007.py
+++ Madgraph/MadGraph-JobOptions/Signal-processes-no-cuts/100007/mc.aMC_qq_H-100007.py
@@ -32,10 +32,10 @@ import model SMEFTatNLO-NLO
 define p = g u c d s b u~ c~ d~ s~ b~
 define j = g u c d s b u~ c~ d~ s~ b~
 define top = t t~
-generate p p > h > j j e- ve~ mu+ vm $$ z NP==2 QCD=0 QED==6 / top
-add process p p > h > j j e+ ve mu- vm~ $$ z NP==2 QCD=0 QED==6 / top
-add process p p > h > j j e+ ve e- ve~ $$ z NP==2 QCD=0 QED==6 / top
-add process p p > h > j j mu+ vm mu- vm~ $$ z NP==2 QCD=0 QED==6 / top
+generate p p > h > j j e- ve~ mu+ vm $$ z NP=2 QCD=0 QED==6 / top
+add process p p > h > j j e+ ve mu- vm~ $$ z NP=2 QCD=0 QED==6 / top
+add process p p > h > j j e+ ve e- ve~ $$ z NP=2 QCD=0 QED==6 / top
+add process p p > h > j j mu+ vm mu- vm~ $$ z NP=2 QCD=0 QED==6 / top
 output -f"""
 
 
